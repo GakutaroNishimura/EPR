8e2f6091e62827f19298310cc9261330946b222a
GakutaroNishimura <nishi.g@quark.ap.titech.ac.jp>
2023-11-28 11:06:37 +0900

    polarization.pyのHe関連のパラメータ変えた.

--- polarization.py
+++ polarization.py
@@ -5,7 +5,7 @@ import sys
 argvs = sys.argv
 path = "./FreqValue/"
 
-T = 0 
+T = 200 
 mu_0 = 1.25663*10**(-6)          #[m kg s^-2 A^-2]
 mu_B = 9.27401*10**(-24)         #[J T^-1] = [m^2 A]
 mu_He = -1.07461*10**(-26)       #[J T^-1] = [m^2 A]
@@ -17,10 +17,11 @@ g_e = -2.00231
 #g_J = 2.002332
 I = 5/2
 A = 1.01191*10**9                #[s^-1]
-He_num = 2.68678*3.3*10**25      #[m^-3]
+He_num = 2.68678*3.0*10**25      #[m^-3]
 Xe_num = 5.88*10**3*6.02*10**23/131.3           #[m^-3]
 B = 1.5*10**(-3)                 #[T]
 kappa_Xe = 518
+kappa_He = 4.52+0.00934*T
 h = 6.62607*10**(-34)  #[J s]
 
 def EPRshift(bfs, bff, afs, aff):
@@ -38,12 +39,18 @@ def EPRshift(bfs, bff, afs, aff):
     return df1_mean, df2_mean, delta_EPR, delta_EPR_std
 
 
-def CalPol(delta_EPR, delta_EPR_std):
+def CalPolXe(delta_EPR, delta_EPR_std):
     abs_Pol = 100*delta_EPR/(2*(2*mu_0/3)*(mu_B*g_e/(h*(2*I+1)))*(1-(8*I/(2*I+1)**2)*(mu_B*g_e*B/(h*A)))*kappa_Xe*mu_Xe_129*Xe_num)
     abs_Pol_std = 100*delta_EPR_std/(2*(2*mu_0/3)*(mu_B*g_e/(h*(2*I+1)))*(1-(8*I/(2*I+1)**2)*(mu_B*g_e*B/(h*A)))*kappa_Xe*mu_Xe_129*Xe_num)
 
     return abs_Pol, abs_Pol_std
 
+
+def CalPolHe(delta_EPR, delta_EPR_std):
+    abs_Pol = 100*delta_EPR/(2*(2*mu_0/3)*(mu_B*g_e/(h*(2*I+1)))*(1-(8*I/(2*I+1)**2)*(mu_B*g_e*B/(h*A)))*kappa_He*mu_He*He_num)
+    abs_Pol_std = 100*delta_EPR_std/(2*(2*mu_0/3)*(mu_B*g_e/(h*(2*I+1)))*(1-(8*I/(2*I+1)**2)*(mu_B*g_e*B/(h*A)))*kappa_He*mu_He*He_num)
+
+    return abs_Pol, abs_Pol_std
 
 if __name__ == "__main__":
     bfs = input("flip前のスタート地点を入力してください : ")
@@ -59,7 +66,8 @@ if __name__ == "__main__":
 
     print("bfEPRfreq. : %f   afEPRfreq. : %f  delta_EPR : %f  delta_EPR_std : %f" %(df1_mean, df2_mean, delta_EPR, delta_EPR_std))
 
-    abs_pol, abs_pol_std = CalPol(delta_EPR, delta_EPR_std)
+    #abs_pol, abs_pol_std = CalPolXe(delta_EPR, delta_EPR_std)
+    abs_pol, abs_pol_std = CalPolHe(delta_EPR, delta_EPR_std)
 
     print("abs_pol : %f    abs_pol_std : %f" %(abs_pol, abs_pol_std))
 
